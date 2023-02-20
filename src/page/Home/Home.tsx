b8a08e39323db394ccab865d9ff59faf9c0cb9d0
rhesus2017 <rhesus2016@gmail.com>
2023-02-20 18:01:13 +0900

    feature: 로그인, 회원가입 모달창 생성

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
 import styled from "styled-components";
+import JoinModal from "../../components/JoinModal";
+import LoginModal from "../../components/LoginModal";
 
 const Home = () => {
+  const [loginModalVisible, setLoginModalVisible] = useState(false);
+  const [joinModalVisible, setJoinModalVisible] = useState(false);
+
   return (
     <HomeStyled>
-      <div>12</div>
+      <p>김태진</p>
+      <button type="button" onClick={() => setLoginModalVisible(true)}>
+        로그인하기
+      </button>
+      {loginModalVisible && (
+        <LoginModal
+          setVisible={setLoginModalVisible}
+          setJoinModalVisible={setJoinModalVisible}
+        />
+      )}
+      {joinModalVisible && (
+        <JoinModal
+          visible={joinModalVisible}
+          setVisible={setJoinModalVisible}
+        />
+      )}
     </HomeStyled>
   );
 };
 
 export default Home;
 
-const HomeStyled = styled.div``;
+const HomeStyled = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+
+  > p {
+    padding-top: 32px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    text-align: center;
+    letter-spacing: -0.02em;
+  }
+
+  > button {
+    width: 343px;
+    height: 43px;
+    background: #ff4081;
+    color: #fff;
+    border-radius: 6px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+`;
--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { UserType } from "../interfae/user";
+import { UserType } from "../models/user";
 
 const initialState: UserType = {
   email: "",
