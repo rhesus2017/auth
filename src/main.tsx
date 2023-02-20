ac3b55fc2de17747d18da275ebeb0908416e983e
rhesus2017 <rhesus2016@gmail.com>
2023-02-20 11:47:18 +0900

    feature: 메인 페이지 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../src/assets/css/reset.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
---
+++ src/page/Home/Home.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <HomeStyled>
+      <div>12</div>
+    </HomeStyled>
+  );
+};
+
+export default Home;
+
+const HomeStyled = styled.div``;
